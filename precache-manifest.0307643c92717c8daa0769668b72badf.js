322b314046782687da06c37dc59f8e118d40a731
Паша <holms703@gmail.com>
2020-09-06 05:05:39 +0300

    Updates

---
+++ precache-manifest.0307643c92717c8daa0769668b72badf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6237f53567774963bd860e1dda212984",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "00911a51faf8da0d676d",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.727b824d.chunk.css"
+  },
+  {
+    "revision": "aaa6c397007826aa3e69",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c3ef83a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c3ef83a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00911a51faf8da0d676d",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.b352dadc.chunk.js"
+  },
+  {
+    "revision": "b0a6de624da8d595c80f",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.e613806e.js"
+  }
+]);
